715834c0b9bcfa53eed7ff5eeb5b85c7900e8e18
Udula Ravishan <udula.ravishan.email@gmail.com>
2025-08-06 20:58:35 -0400

    Commented PlayerController.java
    Wrote descriptions for methods and notes on http requests in PlayerController.java

--- src/main/java/com/nbastatexplorer/nbastatexplorer/player/PlayerController.java
+++ src/main/java/com/nbastatexplorer/nbastatexplorer/player/PlayerController.java
@@ -8,21 +8,31 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// how computers talk to each other over web --> when browser, app or frontend wants data it sends http request
+// Parts of http inlude:
+// http method (what type of action your doing --> GET(retireve data) POST(send data to create something PUT(Update existing item) DELETE(remove item)
+// URL (Path) - where your going
+// Headers - metadata (auth info, content type)
+// Body - actual data (used in POST/PUT) --> in this case player data in json
+
 //rest controller handles incoming http requests --> puts in service layer and gives out output
 @RestController //every method returns a object (data - json) instead of view (html) --> view renders user interface
 @RequestMapping(path="/player")//class/method handle http requests that match the path
 //when user visists this url run this method
+//sets a base path for all http requests in controller
 public class PlayerController {
-    private final PlayerService playerService;
+    private final PlayerService playerService; //service layer dependency --> controller uses service class to talk to database
 
 
+    //constructor based dependency injection
     public PlayerController(PlayerService playerService) {
         this.playerService = playerService;
     }
 
+    //handles GET (fetch) /player requests
     @GetMapping
     public List<Player> getPlayers(
-            @RequestParam(required = false) String team,
+            @RequestParam(required = false) String team, //RequestParam gets query paramteres --> used to read key=value pairs in url after ?
             @RequestParam(required = false) String name,
             @RequestParam(required = false) String position){
         if(team != null && position != null){
@@ -38,12 +48,14 @@ public class PlayerController {
         }
     }
 
+    // handles Post(create) /player requests
     @PostMapping
-    public ResponseEntity<Player> addPlayer(@RequestBody Player player){
+    public ResponseEntity<Player> addPlayer(@RequestBody Player player){ //Requestbody converts json in request body to java object - reads http request and turn into a java object
         Player createdPlayer = playerService.addPlayer(player);
         return new  ResponseEntity<>(createdPlayer, HttpStatus.CREATED);
     }
 
+    // Put(uopdate) /player requests
     @PutMapping
     public ResponseEntity<Player> updatePlayer(@RequestBody Player player){
         Player updatedPlayer = playerService.updatePlayer(player);
@@ -54,10 +66,14 @@ public class PlayerController {
         }
     }
 
+    // delete /player
     @DeleteMapping("/{playerName}")
-    public ResponseEntity<String> deletePlayer(@PathVariable String playerName){
+    public ResponseEntity<String> deletePlayer(@PathVariable String playerName){ // Pathvariable when value is part of http url
         playerService.deletePlayer(playerName);
         return new  ResponseEntity<>("Player deleted successfully", HttpStatus.OK);
+
     }
 
 }
+
+
