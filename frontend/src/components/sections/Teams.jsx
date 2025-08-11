7fec4bbe496c23ef4a7aadc32e9c274d6fe98d17
Udula Ravishan <udula.ravishan.email@gmail.com>
2025-08-11 16:55:29 -0400

    Added All Teams for Filter
    Added all teams for filter and each teams image

--- frontend/src/components/sections/Teams.jsx
+++ frontend/src/components/sections/Teams.jsx
@@ -4,13 +4,16 @@ import { useNavigate } from "react-router-dom"
 export const Teams = () =>{
     const navigate = useNavigate();
 
-    const teams = ["LAL"] //list containing list of teams
+    const teams = ["ATL","BOS","BRK","CHO","CHI","CLE","DAL","DEN","DET","GSW","HOU","IND","LAC","LAL","MEM","MIA","MIL","MIN","NOP","NYK","OKC","ORL","PHI","PHO","POR","SAC","SAS","TOR","UTA","WAS"]
+    //list containing list of teams
 
     return(
-        <section id="teams" className="min-h-screen flex justify-center items-center relative">
-            <div className="text-center z-10 px-4 space-x-6">
+        <section id="teams" className="min-h-screen flex justify-center items-center relative mt-20">
+            <div className="flex flex-wrap justify-center gap-6 max-2-5xl mx-auto">
                 {/* /teams/${team} routes to designated team page when button clicked*/}
-                {teams.map((team) => (<button key={team} onClick={() => navigate(`/teams/${team}`)} className="font-bold text-5xl text-white bg-black p-5 rounded-xl hover:bg-orange-700 hover:-translate-y-2 transition">{team}</button>)
+                {teams.map((team) => (<button key={team} onClick={() => navigate(`/teams/${team}`)} className="font-bold text-5xl text-white bg-black p-5 rounded-xl hover:bg-orange-700 hover:-translate-y-2 transition">
+                    <img src={`/logos/${team}.png`}/>
+                </button>)
 
 
                 )}
